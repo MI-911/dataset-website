702245f830e87999e92acc3b2ea7618dce6e572c
Anders Jakobsen <andersjkbsn@gmail.com>
2020-02-26 11:56:44 +0100

    Correctly show images

--- mindreader/src/pages/Statistics.js
+++ mindreader/src/pages/Statistics.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row } from 'react-bootstrap';
 import StatisticsCol from '../components/StatisticsCol';
+import longTail from '../statistics/long-tail.png';
+import coverage from '../statistics/coverage.png';
+
+function LongTail() {
+  return <img src={longTail} alt="Long-tail distribution" />;
+}
+
+function Coverage() {
+  return <img src={coverage} alt="Entity type coverage" />;
+}
 
 const Statistics = () => {
   return (
@@ -13,12 +23,12 @@ const Statistics = () => {
       <Container>
         <Row>
           <StatisticsCol header="Long-tail distribution">
-            <img src="./long-tail.png" alt="Long-tail distribution." />
+            <LongTail />
             <p className="small text-muted">MindReader dataset is limited to binary ratings and items (i.e., movies) for a fair comparison.</p>
           </StatisticsCol>
           
           <StatisticsCol header="Entity type coverage">
-            <img src="./coverage.png" alt="Entity type coverage" />
+            <Coverage />
             <p className="small text-muted">Entities with both binary ratings and "don't know" observations are only counted as binary ratings.</p>
           </StatisticsCol>
         </Row>
