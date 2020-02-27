268ec0a5d637532f8242ea6ae1575dfc2171bce5
minibrams <anders@brams.dk>
2020-02-27 13:31:56 +0100

    Added a usage license

--- mindreader/src/pages/Releases.js
+++ mindreader/src/pages/Releases.js
@@ -28,6 +28,33 @@ const Releases = () => {
         <li><BlankLink href="https://mindreader.tech/api/triples">Knowledge graph triples</BlankLink></li>
         <li><BlankLink href="https://mindreader.tech/api/entities">Knowledge graph entities</BlankLink></li>
       </ul>
+
+      <hr className="pb-5"/> 
+      <h2>Usage License</h2>
+      
+      <p>
+      Neither Aalborg University nor any of the researchers involved can guarantee the correctness of the data, its suitability for any particular purpose, or the validity of results based on the use of the data set. The data set may be used for any research purposes under the following conditions:
+      </p>
+        <ol>
+          <li>
+            The user may not state or imply any endorsement from Aalborg University or the researchers who produced it.
+          </li>
+          <li>
+            The user must acknowledge the use of the data set in publications resulting from the use of the data set (see <Link to='/home'>here</Link> for citation information).
+          </li>
+          <li>
+            The user may redistribute the data set, including transformations, so long as it is distributed under these same license conditions.
+          </li>
+          <li>
+            The user may not use this information for any commercial or revenue-bearing purposes without first obtaining permission from a faculty member of the Department of Computer Science at Aalborg University.
+          </li>
+          <li>
+            In no event shall Aalborg University, its affiliates or employees be liable to you for any damages arising out of the use or inability to use these programs (including but not limited to loss of data or data being rendered inaccurate).
+          </li>
+        </ol>
+      <p>
+      If you have any further questions or comments, please email <strong>someone@aau.dk (TODO: find a real address to put here)</strong>.
+      </p>
     </>
   );
 };
