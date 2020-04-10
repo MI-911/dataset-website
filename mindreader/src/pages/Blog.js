6a92a95bc66471288a82644c078f93c86ba2adb1
Minibrams <anders@brams.dk>
2020-04-10 12:18:56 +0200

    Syntax highlight hacks

--- mindreader/src/pages/Blog.js
+++ mindreader/src/pages/Blog.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import blogPost from "../markdown/Building_a_recommender_with_Neo4j.md";
 import ReactMarkdown from "react-markdown";
+
 import cypher from "highlightjs-cypher";
 import hljs from "highlightjs";
 
@@ -39,24 +40,41 @@ const Blog = () => {
 
   useEffect(() => {
     hljs.initHighlightingOnLoad();
-
     const load = async () => {
       const response = await fetch(blogPost);
       let content = await response.text();
-      // Cleanup
-      content = content.replace("~~~~", "");
 
-      // Convert code to unindented and add cypher language syntax highlighting
+      // Cleanup and add Cypher syntax highlighting
+      content = content.replace("~~~~", "");
       setBlogContent(addCodeLanguage(content));
     };
 
     load();
   }, []);
 
+  useEffect(() => {
+    // Highlight
+    hljs.initHighlightingOnLoad();
+  }, [blogContent]);
+
   return (
     <ReactMarkdown
       className="markdown"
       source={blogContent ?? `# Loading...`}
+      renderers={{
+        // Giga-hack - hijact the Markdown renderer for codeblocks
+        code: (v) => {
+          return (
+            <pre>
+              <code
+                dangerouslySetInnerHTML={{
+                  __html: hljs.highlight(v.language, v.value).value,
+                }}
+              />
+            </pre>
+          );
+        },
+      }}
     />
   );
 };
