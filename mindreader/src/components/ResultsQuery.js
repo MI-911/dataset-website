bc908f7dbfcb25832f04b9b4aa110f0461c00e47
Anders Jakobsen <andersjkbsn@gmail.com>
2020-04-15 16:17:40 +0200

    Changed API

--- mindreader/src/components/ResultsQuery.js
+++ mindreader/src/components/ResultsQuery.js
@@ -2,8 +2,17 @@ import React from "react";
 import axios from 'axios';
 import {Line} from 'react-chartjs-2';
 
-const COLORS = ['#087CB8', '#9b59b6', '#e74c3c', '#dfe70d', '#e7399a', '#2cd5e7', '#e78412', '#e74c3c'];
-const API = 'https://mindreader.tech/spectate-api';
+const COLORS = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+                '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+                '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+                '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+                '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+                '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+const API = 'https://mindreader.tech/spectate';
 
 
 const ComboBox = (props) => {
@@ -82,7 +91,7 @@ class ResultsQuery extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <>
         <div className="form-row">
           <div className="col">
             <ComboBox onChange={this.handleChange} value={this.state.experiment} items={this.state.experiments}
@@ -103,10 +112,10 @@ class ResultsQuery extends React.Component {
           </div>
         </div>
 
-        <div id="container" className="mt-2">
+        <div className="mt-2">
           <Line data={this.state.data} />
         </div>
-      </div>
+      </>
     )
   }
 }
