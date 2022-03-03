MI-911/dataset-website

commit 623a43399a519039b03b388f41c7ecbadc1b6619

Released MIndReader-200K

mindreader/src/pages/Releases.js
@@ -10,29 +10,26 @@ const Releases = () => {
         Statistics on the latest available release are available{" "}
         <Link to="/statistics">here</Link>.
       </p>
-      <h4>MindReader-100K</h4>
-      <p className="small text-muted">Released 2020-02-25</p>
+      <h4>MindReader-200K</h4>
+      <p className="small text-muted">Released 2020-09-09</p>
       <p>
-        The latest stable release of MindReader is MindReader-100K with 102,160
-        ratings from 1,174 users over 10,030 entities. We recommend this dataset
-        for use in research, as it will be permanently available on this page.
-        Furthermore, we provide a variant containing only ratings from users who
-        reached the final page of the quiz denoted by 'final only'.
+        The latest stable release of MindReader containing 218,794 ratings from 2,316 users
+        over 12,206 entities. We recommend this dataset for use in research, as it will be
+        permanently available on this page. Furthermore, we provide a variant containing only
+        ratings from users who reached the final page of the quiz denoted by 'final only'.
         Links to download the dataset and associated knowledge graph triples can
         be found below.
       </p>
       <ul>
         <li>
-          <BlankLink href="https://mindreader.tech/api/ratings?versions=100k,100k-newer,100k-fix">
+          <BlankLink href="https://mindreader.tech/api/statistics?versions=100k,100k-newer,100k-fix,thesis,thesis-ppr,thesis-launch">
             Ratings
-          </BlankLink>{" "}
-          (8,6 MB)
+          </BlankLink>
         </li>
         <li>
-          <BlankLink href="https://mindreader.tech/api/ratings?versions=100k,100k-newer,100k-fix&final=yes">
+          <BlankLink href="https://mindreader.tech/api/statistics?versions=100k,100k-newer,100k-fix,thesis,thesis-ppr,thesis-launch&final=yes">
             Ratings (final only)
-          </BlankLink>{" "}
-          (7,6 MB)
+          </BlankLink>
         </li>
         <li>
           <BlankLink href="https://mindreader.tech/neo4j_graph.csv">
@@ -51,6 +48,27 @@ const Releases = () => {
             Knowledge graph (entities only)
           </BlankLink>{" "}
           (1.2 MB)
+        </li>
+      </ul>
+      <hr />
+
+      <h4>MindReader-100K</h4>
+      <p className="small text-muted">Released 2020-02-25</p>
+      <p>
+        Contains 102,160 ratings from 1,174 users over 10,030 entities.
+      </p>
+      <ul>
+        <li>
+          <BlankLink href="https://mindreader.tech/api/ratings?versions=100k,100k-newer,100k-fix">
+            Ratings
+          </BlankLink>{" "}
+          (8,6 MB)
+        </li>
+        <li>
+          <BlankLink href="https://mindreader.tech/api/ratings?versions=100k,100k-newer,100k-fix&final=yes">
+            Ratings (final only)
+          </BlankLink>{" "}
+          (7,6 MB)
         </li>
       </ul>
       <hr />

mindreader/src/pages/About.js
@@ -12,7 +12,7 @@ const About = () => {
       <p>
         MindReader is a novel dataset providing explicit user ratings over a
         knowledge graph within the movie domain. The latest stable version of
-        the dataset contains 102,160 ratings from 1,174 users over 10,030
+        the dataset contains 218,794 ratings from 2,316 users over 12,206 entities
         entities, and an associated knowledge graph consisting of 18,133
         movie-related entities. The dataset is collected from an online movie
         recommendation game,{" "}
@@ -28,6 +28,17 @@ const About = () => {
           <button className="mr-btn mr-btn-lg">Download</button>
         </Link>
       </div>
+
+      <h4>Reference</h4>
+      <hr />
+      <p>
+        Guidelines for referencing the dataset will be present prior to
+        publication.
+      </p>
+
+      <p>
+        An extended edition of the white paper is available <BlankLink href="https://mindreader.tech/mindreader_extended.pdf">here</BlankLink>.
+      </p>
 
       <h4>Releases</h4>
       <hr />
@@ -210,17 +221,6 @@ const About = () => {
         </BlankLink>
         . Further instructions on partitioning the dataset and conducting the
         experiments are available in the repository.
-      </p>
-
-      <h4>Reference</h4>
-      <hr />
-      <p>
-        Guidelines for referencing the dataset will be present prior to
-        publication.
-      </p>
-
-      <p>
-        An extended edition of the white paper is available <BlankLink href="https://mindreader.tech/mindreader_extended.pdf">here</BlankLink>.
       </p>
       
       <h4>Acknowledgements</h4>

commit 036ef711c478b721441dba5fbf952ea5856b28ea

Fixed bug introduced in 623a43399a519039b03b388f41c7ecbadc1b6619

mindreader/src/pages/Releases.js
@@ -22,12 +22,12 @@ const Releases = () => {
       </p>
       <ul>
         <li>
-          <BlankLink href="https://mindreader.tech/api/statistics?versions=100k,100k-newer,100k-fix,thesis,thesis-ppr,thesis-launch">
+          <BlankLink href="https://mindreader.tech/api/ratings?versions=100k,100k-newer,100k-fix,thesis,thesis-ppr,thesis-launch">
             Ratings
           </BlankLink>
         </li>
         <li>
-          <BlankLink href="https://mindreader.tech/api/statistics?versions=100k,100k-newer,100k-fix,thesis,thesis-ppr,thesis-launch&final=yes">
+          <BlankLink href="https://mindreader.tech/api/ratings?versions=100k,100k-newer,100k-fix,thesis,thesis-ppr,thesis-launch&final=yes">
             Ratings (final only)
           </BlankLink>
         </li>